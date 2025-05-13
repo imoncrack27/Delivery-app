fbb9aae2c1f1f5dd91f3431fd05e795f0e42587c
imoncrack27 <mingodrexell321@gmail.com>
2025-05-13 14:36:13 +0800

    added user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,20 @@
+// server/models/User.js
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["customer", "restaurant"],
+      default: "customer",
+    },
+  },
+  { timestamps: true } // Automatically add createdAt and updatedAt fields
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
