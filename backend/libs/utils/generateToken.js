ff6139634244e5e4c2220b839337d72b9aba1e90
imoncrack27 <mingodrexell321@gmail.com>
2025-05-14 15:22:00 +0800

    changed into development but changing it into production later on

--- backend/libs/utils/generateToken.js
+++ backend/libs/utils/generateToken.js
@@ -7,6 +7,8 @@ export const generateTokenAndSetCookie = (userId, res) => {
 
   res.cookie("jwt", token, {
     httpOnly: true, //Prevents XSS attacks cross-site scripting attacks
+
+    // TODO: Set secure to true in production
     secure: process.env.NODE_ENV !== "development",
     sameSite: "strict", // CSRF attacks cross-site request forgery attacks
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
