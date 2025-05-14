bea6154ea27bde63a4315bc69ad8ebe4a1cc827f
imoncrack27 <mingodrexell321@gmail.com>
2025-05-14 16:04:31 +0800

    installed tailwind css with postcss version 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
-    port: 3000,
+    port: 3000, // Port for the frontend (React app)
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: false,
+        target: "http://localhost:5000", // Backend URL
+        changeOrigin: true, // Adjusts the origin header to match the target URL
+        secure: false, // Disable SSL verification for local development
       },
     },
   },
