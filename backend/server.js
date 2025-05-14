63ec6e272d31494232aadfb8e66e3f5e06b19977
imoncrack27 <mingodrexell321@gmail.com>
2025-05-14 15:21:31 +0800

    added cookie parser

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import connectMongoDB from "./db/connectMongoDB.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -13,6 +15,8 @@ const PORT = process.env.PORT || 5000; // Default to 5000 if PORT isn't set
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.use(cookieParser()); // to parse cookies
 
 //Route Middleware
 app.use("/api/auth", authRoutes); // Use auth routes for authentication-related endpoints
